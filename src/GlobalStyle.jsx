3fb7a3eb3b6f7eab6d9d11cb728141a10707a4f7
seonik <61347719+ikik-pd@users.noreply.github.com>
2023-06-29 10:02:32 +0900

    Revert "Feat. login 회원가입, 로그인, 로그아웃 완료"

--- src/GlobalStyle.jsx
+++ src/GlobalStyle.jsx
@@ -1,10 +1,9 @@
-import { createGlobalStyle } from "styled-components";
+import {createGlobalStyle} from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
     body {
         font-family: "";
         line-height: 1.5;
-        font-size: 16px;
     }
 `;
 
